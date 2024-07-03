ee629abf0cd0407b0eed9619b6aa8769a39e4989
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-07-03 16:32:31 +0500

    5-lesson

--- src/App.jsx
+++ src/App.jsx
@@ -84,16 +84,21 @@ class App extends React.Component {
       ]
     }
     this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
   }
   render() {
       return (
         <div className="wrapper">
-          <Header orders={this.state.orders}/>
+          <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
           <Items items={this.state.item} onAdd={this.addToOrder} />
           <Footer/>
         </div>
       )
 
+  }
+
+  deleteOrder(id) {
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
   }
 
   addToOrder(item) {
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { FaTrash } from "react-icons/fa";
+
 
 export class Order extends Component {
   render() {
@@ -7,6 +9,7 @@ export class Order extends Component {
           <img src={this.props.item.img} alt="img" />
           <h2>{this.props.item.title}</h2>
           <p>{this.props.item.price}$</p>
+          <FaTrash className='delete-ican' onClick={() =>this.props.onDelete(this.props.item.id)}/>
       </div>
     )
   }
