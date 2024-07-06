a4883473f1b508cd1459bf9565df992ec59cf7a5
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2024-07-06 20:20:01 +0500

    6-lesson

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Items from "./components/Items"
 import Categories from "./components/Categories"
+import ShowFullItem from "./components/ShowFullItem"
 
 class App extends React.Component {
   constructor(props) {
@@ -83,23 +84,33 @@ class App extends React.Component {
         category: 'dresser',
         price: '100.99'
       },
-      ]
+      ],
+      showFullItem: false,
+      fullItem: {}
     }
     this.state.currentItems = this.state.item;
     this.addToOrder = this.addToOrder.bind(this);
     this.deleteOrder = this.deleteOrder.bind(this);
     this.chooseCategory = this.chooseCategory.bind(this);
+    this.onShowItem = this.onShowItem.bind(this);
   }
   render() {
       return (
         <div className="wrapper">
           <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
           <Categories chooseCategory={this.chooseCategory}/>
-          <Items items={this.state.currentItems} onAdd={this.addToOrder} />
+          <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder} />
+
+          {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem} />}
           <Footer/>
         </div>
       )
 
+  }
+
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem : !this.state.showFullItem})
   }
 
   chooseCategory(category) {
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ export class Item extends Component {
     render() {
         return(
             <div className="item">
-                <img src={this.props.item.img} alt="img" />
+                <img src={this.props.item.img} alt="img" onClick={() => this.props.onShowItem(this.props.item)} />
                 <h2>{this.props.item.title}</h2>
                 <p>{this.props.item.desc}</p>
                 <b>{this.props.item.price}$</b>
